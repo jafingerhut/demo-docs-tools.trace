94f28fc46b40724709919c71837cdf832709cab1
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-09-08 13:41:20 -0700

    Add many comments about whether tools.trace prints tracing message for various cases

--- src/demo1/ns1.clj
+++ src/demo1/ns1.clj
@@ -9,7 +9,7 @@
   (foo [this])
   (bar-me [this] [this y]))
 
-(deftype Demo1Type [a b c] 
+(deftype Demo1Type [a b c]
   Demo1Proto1
   (foo [this] a)
   (bar-me [this] b)
@@ -38,9 +38,31 @@
   (guh [this y] {:guh-on-long this :y y}))
 
 
+(deftype Demo1OtherType [w x])
+
+(extend-type Demo1OtherType
+  Demo1Proto1
+  (foo [this] {:called :Demo1Proto1-foo :this this})
+  ;; multi-arity function definitions must be combined inside of
+  ;; extend-type.  Clojure compiler will not give any error if you
+  ;; instead define them separately, but it will silently overwrite
+  ;; the earlier definition with the later one.
+  (bar-me
+    ([this]
+     {:called :Demo1Proto1-bar-me-arity-1 :this this})
+    ([this y]
+     {:called :Demo1Proto1-bar-me-arity-2 :this this :y y}))
+
+  Demo1Proto2
+  (baz [this] {:called :Demo1Proto2-baz :this this})
+  (guh [this y] {:called :Demo1Proto2-guh :this this :y y}))
+
+
 (defrecord Demo1Rec1 [a b]
   Demo1Proto1
   (foo [this] {:called :Demo1Proto1-foo :this this})
+  ;; multi-arity function definitions must be separate inside of
+  ;; defrecord.
   ;;(bar-me
   ;;  ([this] (* b 9))
   ;;  ([this y] (* a b y))
@@ -92,22 +114,52 @@
 (t/trace-ns 'd) ;; namespace alias as symbol
 
 
+;; With Oracle JDK 8, Clojure 1.9.0, tools.trace 0.7.9, whether trace
+;; messages appeared or not is in comments marked "v1".
+
+;; v1: no trace messages for protocol method calls on Demo1Type
 (def d1 (demo1.ns1.Demo1Type. 5 10 15))
 (d/foo d1)
 (d/bar-me d1)
 (d/bar-me d1 -1)
 
+;; v1: yes trace message for unimplemented protocol method call on
+;; Demo1Type, at least for the call, but not return, because exception
+;; thrown before return.
+(d/baz d1)
+(d/guh d1 5)
+
+;; v1: yes trace messages for protocol method calls on java.lang.Long
 (d/foo 100)
 (d/bar-me 100)
 (d/bar-me 100 8)
-
 (d/baz 100)
 (d/guh 100 5)
 
+;; v1: yes trace messages for protocol method calls on Demo1OtherType,
+;; where all of the implementations were given using extend-type.
+(def d2 (demo1.ns1.Demo1OtherType. 31 63))
+(d/foo d2)
+(d/bar-me d2)
+(d/bar-me d2 -1)
+(d/baz d2)
+(d/guh d2 5)
+
+
+;; v1: yes trace message for record constructor functions ->Demo1Rec1
+;; and map->Demo1Rec1
 (def r1 (d/->Demo1Rec1 17 19))
+(def r2 (d/map->Demo1Rec1 {:a "a" :b "b"}))
+
+;; v1: no trace messages for protocol method calls on a record, where
+;; the method implementations were given within the defrecord form.
 (d/foo r1)
 (d/bar-me r1)
 (d/bar-me r1 23)
+
+;; v1: yes trace messages for protocol method calls on a record, where
+;; the method implementations were given within an extend-type form
+;; after defrecord and defprotocol.
 (d/baz r1)
 (d/guh r1 37)
 
